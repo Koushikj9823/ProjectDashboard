352fd2ce965ba4ce6dc7e6b8033f50736fb82ff9
J, Koushik <Koushik.J@sabre.com>
2020-08-09 19:51:52 +0530

    React Build added

---
+++ src/main/resources/static/precache-manifest.21c6d117ab6cc4cc1bc1c69592827139.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ce46e34d7f81ca29ac93e89c6bb163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "857c861e59fd6f18c471",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d843e4d9602e74dc44e4",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "857c861e59fd6f18c471",
+    "url": "/static/js/2.24de5609.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.24de5609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d843e4d9602e74dc44e4",
+    "url": "/static/js/main.4a3320fd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
