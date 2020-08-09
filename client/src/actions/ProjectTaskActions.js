6f91e2e0390f6cfa37a18de0e597c3277f96d92d
J, Koushik <Koushik.J@sabre.com>
2020-08-09 19:40:26 +0530

    Added the Url for Heroku

--- client/src/actions/ProjectTaskActions.js
+++ client/src/actions/ProjectTaskActions.js
@@ -4,7 +4,7 @@ import {DELETE_PROJECT_TASKS, GET_ERRORS, GET_PROJECT_TASK, GET_PROJECT_TASKS} f
 export const addProjectTask = (project_task,history) => async dispatch =>{
 
     try{
-        await axios.post("http://localhost:8090/api/board",project_task);
+        await axios.post("/api/board",project_task);
         history.push("/");
         dispatch({
             type:GET_ERRORS,
@@ -20,7 +20,7 @@ export const addProjectTask = (project_task,history) => async dispatch =>{
 };
 
 export const getBacklog = () => async dispatch => {
-    const res = await axios.get("http://localhost:8090/api/board/all")
+    const res = await axios.get("/api/board/all");
     dispatch({
         type:GET_PROJECT_TASKS,
         payload:res.data
@@ -30,7 +30,7 @@ export const getBacklog = () => async dispatch => {
 export const deleteProjectTask = pt_id => async dispatch => {
     if(window.confirm('Are you sure you want to delete Project Task ' + pt_id)
     ) {
-        await axios.delete("http://localhost:8090/api/board/"+pt_id);
+        await axios.delete("/api/board/"+pt_id);
         dispatch({
             type: DELETE_PROJECT_TASKS,
             payload:pt_id
@@ -40,7 +40,7 @@ export const deleteProjectTask = pt_id => async dispatch => {
 
 export const getProjectTask = (pt_id,history) => async dispatch => {
     try{
-        const res = await axios.get("http://localhost:8090/api/board/"+pt_id);
+        const res = await axios.get("/api/board/"+pt_id);
         dispatch({
             type: GET_PROJECT_TASK,
             payload:res.data
